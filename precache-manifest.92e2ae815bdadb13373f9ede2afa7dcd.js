c8b614524871e8cea49077e34e567704a374fe03
Santiago M <santiagorestrepo28@hotmail.com>
2019-11-22 16:25:18 -0500

    Updates

---
+++ precache-manifest.92e2ae815bdadb13373f9ede2afa7dcd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045d185ba1abe847395c9ccf0d9eaad1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba3dfec5134967cecf26",
+    "url": "/static/css/main.64a35e11.chunk.css"
+  },
+  {
+    "revision": "ddac9c9706c06235cad0",
+    "url": "/static/js/2.ae0f0866.chunk.js"
+  },
+  {
+    "revision": "ba3dfec5134967cecf26",
+    "url": "/static/js/main.e2670b47.chunk.js"
+  },
+  {
+    "revision": "8aed5aac1ba52abaedc9",
+    "url": "/static/js/runtime-main.ebe0d3ae.js"
+  },
+  {
+    "revision": "af796b4f4e564a0dcdd5e64b76e3afee",
+    "url": "/static/media/Camilo.af796b4f.jpg"
+  },
+  {
+    "revision": "f1b3deed82dab370869c299ad616d0ed",
+    "url": "/static/media/Carro_lavando1.f1b3deed.jpg"
+  },
+  {
+    "revision": "828bbf35a932e121b682b0882b96590b",
+    "url": "/static/media/Daniel.828bbf35.jpg"
+  },
+  {
+    "revision": "f0d4dba488f61740bd165e94bb26f6ab",
+    "url": "/static/media/Santiago.f0d4dba4.jpg"
+  },
+  {
+    "revision": "ae0f4ccfb640eb66615e27fad9545f74",
+    "url": "/static/media/Vision.ae0f4ccf.jpg"
+  },
+  {
+    "revision": "21d9541be2078df5fa4a0d8d64a08804",
+    "url": "/static/media/background-parking.21d9541b.jpg"
+  },
+  {
+    "revision": "477abacab4ab92464805eb162c313098",
+    "url": "/static/media/carrobrillante.477abaca.jpg"
+  },
+  {
+    "revision": "0ac519f8140c84e26c7955184eaffc9e",
+    "url": "/static/media/images.0ac519f8.jpg"
+  },
+  {
+    "revision": "8ff6f6cc693385dcfed98f5407a18b9f",
+    "url": "/static/media/mision.8ff6f6cc.jpg"
+  },
+  {
+    "revision": "91c6fbd4faf556ada6f7b7a8efc71fc8",
+    "url": "/static/media/parqueo.91c6fbd4.jpg"
+  },
+  {
+    "revision": "21d37a2c982d65d14ce43be07d486fa9",
+    "url": "/static/media/pisos.21d37a2c.jpg"
+  },
+  {
+    "revision": "9faef497513644377627353dea79ffd4",
+    "url": "/static/media/register.9faef497.png"
+  },
+  {
+    "revision": "2158930aa723a1bbdaa4bf6945fc25c9",
+    "url": "/static/media/security-register.2158930a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hello/precache-manifest.dcb876b9b8eac38c22864ada65372de8.js"
+  "/precache-manifest.92e2ae815bdadb13373f9ede2afa7dcd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hello/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
